6c7a8b14e0653a2acce29bedc6df39ee6ccdf88f
greenywd <greenydev@greenmail.net.au>
2020-05-05 15:39:53 +0930

    Add --check to check for worlds before converting

--- convert.py
+++ convert.py
@@ -16,7 +16,7 @@ class EndNotFound(WorldConverterError):
         self.message = message
 
 class WorldConverter:
-    def checkWorldPath(self, path: str, world_name: str = "world"):
+    def checkWorld(self, path: str, world_name: str = "world"):
         # Add a trailing / if one isn't given
         if (path[-1:] != '/'):
             path += '/'
@@ -29,14 +29,14 @@ class WorldConverter:
             if (not os.path.exists(path + world_name + '_the_end')):
                 raise EndNotFound("The End not found in %s. Make sure that %s_the_end exists or try specifying the world name with --world-name." % (path, world_name))
 
-            print("Nether and The End worlds found.")
+            print("Nether and The End worlds found. Ready to convert!")
 
         else:
             raise FileNotFoundError("Directory '%s' does not exist." % (path)) 
 
     def convertWorld(self, path: str, world_name: str = "world"):
         try:
-            self.checkWorldPath(path, world_name)
+            self.checkWorld(path, world_name)
         except Exception as error:
             print(error)
             sys.exit(1)
@@ -48,8 +48,9 @@ class WorldConverter:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Bukkit World Converter', description='Convert Bukkit Server worlds to Singleplayer worlds.', epilog='If no arguments are given, nothing will happen.')
-    parser.add_argument('--world-dir', type=str, required=True, metavar='<world dir>', help='The directory that the Bukkit world is stored.')
-    parser.add_argument('--world-name', type=str, required=True, metavar='<world>', help='The name of the world.')
+    parser.add_argument('--world-dir', type=str, required=False, metavar='<world dir>', help='The directory that the Bukkit world is stored.')
+    parser.add_argument('--world-name', type=str, required=False, metavar='<world>', help='The name of the world.')
+    parser.add_argument('--check-world', type=str, required=False, metavar='<world dir>', help='Check whether all worlds exist to convert.')
     args = parser.parse_args()
 
     converter = WorldConverter()
@@ -61,3 +62,14 @@ if __name__ == "__main__":
             print(error)
         except EndNotFound as error:
             print(error)
+
+        sys.exit(0)
+
+    if args.check_world:
+        try:
+            converter.checkWorld(args.check_world)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+        
+        sys.exit(0)
